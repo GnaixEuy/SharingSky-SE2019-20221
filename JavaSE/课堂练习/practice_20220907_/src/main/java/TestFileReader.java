1ab6dc6757c0eee72ea0b29994033c7bae636951
GnaixEuy <sgghcj@live.com>
2022-09-07 10:42:52 +0800

    :beer: 提交20220907课堂练习

---
+++ JavaSE/课堂练习/practice_20220907_/src/main/java/TestFileReader.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+/**
+ * <img src="http://blog.gnaixeuy.cn/wp-content/uploads/2022/06/bug.png"/>
+ *
+ * <p>项目： SharingSky-SE2019-20221 </p>
+ *
+ * @author GnaixEuy
+ * @date 2022/9/7
+ * @see <a href="https://github.com/GnaixEuy"> GnaixEuy的GitHub </a>
+ */
+public class TestFileReader {
+
+    public static void main(String[] args) {
+        FileReader fileReader = null;
+        BufferedReader bufferedReader = null;
+        try {
+            fileReader = new FileReader("/Users/gnaixeuy/Desktop/SharingSky-SE2019-20221/JavaSE/课堂练习/practice_20220907_/src/main/resources/myInfo.txt");
+            bufferedReader = new BufferedReader(fileReader);
+            String str = bufferedReader.readLine();
+            while (str != null) {
+                System.out.println(str);
+                str = bufferedReader.readLine();
+            }
+            fileReader.close();
+            bufferedReader.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}
